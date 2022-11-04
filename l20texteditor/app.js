54ccf27d40b981ffc532255cbb193d7492667916
thawkhant <91838782+thawkhant@users.noreply.github.com>
2022-11-04 15:58:33 +0630

    i have record

--- l20texteditor/app.js
+++ l20texteditor/app.js
@@ -1,45 +1,96 @@
 var gettxtarea = document.getElementById("textarea");
+var getdivarea = document.getElementById("divarea");
+getdivarea.contentEditable = true;
+getdivarea.spellcheck = false;
 
-function boldfun(){
-    gettxtarea.style.fontWeight = "bold";
-}
 
-function italicfun(){
-	gettxtarea.style.fontStyle = "italic";
-}
+var getbtns = document.querySelectorAll(".btn");
+// console.log(getbtns) // Node List
 
-function underlinefun(){
-	gettxtarea.style.textDecoration = "underline";
-}
+getbtns.forEach(function(getbtn){
+	getbtn.addEventListener("click",function(){
+		// var getcommand = getbtn.getAttribute("data-command");
+		var getcommand = getbtn.dataset['abc'];    // data ko direct lan call lot ma ya lot pr sir
+       // console.log(getcommand);
 
-function laglgfun(){
-	gettxtarea.style.textAlign= "left";
-}
+       if(getcommand === "cleartext"){
+         getdivarea.innerHTML = "" ;    // div htal ka har dwe ko delete chin yin value net ma ya buu naw bro
+       }else if(getcommand === "createLink" || getcommand === "insertImage"){
+       	                       // message       , default
+         let geturl = prompt("Enter your website link","https://");
+          document.execCommand(getcommand,false,geturl);
+       }else if(getcommand==="foreColor"){
+         // console.log(getbtn.value);
+          document.execCommand(getcommand,false,getbtn.value);
+       }else if(getcommand==="backColor"){
+           document.execCommand(getcommand,false,getbtn.value);
+       }else if(getcommand === "fontName"){
+           document.execCommand(getcommand,false,getbtn.value);
+       }else if(getcommand === "paste"){
+           navigator.clipboard.readText().then(function(cliptext){    // de har lay ka bal ko phar thar yae ya dar
+             // console.log(cliptext)
+             getdivarea.innerHTML +="cliptext";  // appendd loke lite dar
+           });
+       }else{
+        document.execCommand(getcommand,false,null);
+       }
 
-function calgfun(){
-	gettxtarea.style.textAlign = "center";
-}
+      
+	});
+});
 
-function ralgfun(){
-	gettxtarea.style.textAlign = "right";
-}
+// function boldfun(){
+//     gettxtarea.style.fontWeight = "bold";
+// }
 
-function upcasefun(){
-	gettxtarea.style.textTransform = "uppercase";
-}
+// function italicfun(){
+// 	gettxtarea.style.fontStyle = "italic";
+// }
 
-function lwcase(){
-	gettxtarea.style.textTransform = "lowercase";
-}
+// function underlinefun(){
+// 	gettxtarea.style.textDecoration = "underline";
+// }
 
-function capcasefun(){
-	gettxtarea.style.textTransform = "capitalize";
-}
+// function laglgfun(){
+// 	gettxtarea.style.textAlign= "left";
+// }
 
-function clearfun(){
-	gettxtarea.style.fontWeight = "normal";
-	gettxtarea.style.fontStyle = "normal";
-	gettxtarea.style.textDecoration = "none"
-	gettxtarea.style.textAlign = "left";
-	gettxtarea.value = "";
-}
+// function calgfun(){
+// 	gettxtarea.style.textAlign = "center";
+// }
+
+// function ralgfun(){
+// 	gettxtarea.style.textAlign = "right";
+// }
+
+// function upcasefun(){
+// 	gettxtarea.style.textTransform = "uppercase";
+// }
+
+// function lwcase(){
+// 	gettxtarea.style.textTransform = "lowercase";
+// }
+
+// function capcasefun(){
+// 	gettxtarea.style.textTransform = "capitalize";
+// }
+
+// function clearfun(){
+// 	gettxtarea.style.fontWeight = "normal";
+// 	gettxtarea.style.fontStyle = "normal";
+// 	gettxtarea.style.textDecoration = "none"
+// 	gettxtarea.style.textAlign = "left";
+// 	gettxtarea.value = "";
+// }
+
+
+
+
+
+// execCommand(aCommandName,aShowDefaultUI,aValueArgument);
+
+//  execCommand(aCommandName,false,);
+
+// aShowDefaultUI  // browser history ko check pay dar // exp paste dot lo command twe so yin kwint ma pyu lot 
+// true
+// false
